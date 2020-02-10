44b08f4bc5ed3ff17b83d1ac620b1a340d74879c
Abdulmateen Tairu <tay2druh@gmail.com>
2020-02-10 11:37:07 +0100

    added signal for Token

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+
+def create_token(sender, **kwargs):
+    if kwargs['created']:
+        Token.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_token, User)
